310757cfb0066c9757e562638935670c336a274e
Troels Dalsgaard Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2023-08-31 00:37:52 +0200

    Add ability to specify address, can be used for remote sessions

--- src/bin/dterm/main.rs
+++ src/bin/dterm/main.rs
@@ -6,7 +6,7 @@ pub mod stateful_tree;
 pub mod ui;
 
 use app::{run_app, App};
-use clap::{command, Parser, ValueEnum};
+use clap::{command, Parser, ValueEnum, ArgGroup};
 use crossterm::{
     event::{DisableMouseCapture, EnableMouseCapture},
     execute,
@@ -23,7 +23,7 @@ use tui::{
     Terminal,
 };
 
-use zbus::Connection;
+use zbus::{Connection, ConnectionBuilder, Address};
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum BusType {
@@ -32,33 +32,50 @@ enum BusType {
 }
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
+#[clap(group(ArgGroup::new("bus_or_address").args(&["bus", "address"])))]
 struct Args {
     //Which bus to connect to
     #[clap(default_value_t = BusType::System)]
     #[arg(value_enum)]
     bus: BusType,
+
+    //Address of potentially remote connection
+    #[clap(long)]
+    address: Option<String>
+    
 }
 
+// This function is mainly used to make error handling nicer, so that we can cleanup the terminal nicely
+async fn run<B>(terminal: &mut Terminal<B>, args: Args) -> Result<(), zbus::Error>
+    where B:Backend
+{
+    // create app and run it
+    let tick_rate = Duration::from_millis(250);
+    let mut connection = match args.bus {
+        BusType::System => Connection::system().await?,
+        BusType::Session => Connection::session().await?,
+    };
+    if let Some(address) = args.address {
+        connection = ConnectionBuilder::address(address.as_str())?.build().await?;
+    }
+    let (dbus_handler_sender, app_receiver) = mpsc::channel::<AppMessage>(16);
+    let dbus_handler = DbusActorHandle::new(dbus_handler_sender, connection);
+    // setup terminal
+    let app = App::new(app_receiver, dbus_handler);
+    run_app(terminal, app, tick_rate).await
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    // setup terminal
+
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    // create app and run it
-    let tick_rate = Duration::from_millis(250);
-    let connection = match args.bus {
-        BusType::System => Connection::system().await?,
-        BusType::Session => Connection::session().await?,
-    };
-    let (dbus_handler_sender, app_receiver) = mpsc::channel::<AppMessage>(16);
-    let dbus_handler = DbusActorHandle::new(dbus_handler_sender, connection);
-    let app = App::new(app_receiver, dbus_handler);
-    let res = run_app(&mut terminal, app, tick_rate).await;
+    let res = run(&mut terminal, args).await;
+
     // restore terminal
     disable_raw_mode()?;
     execute!(
@@ -68,9 +85,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     )?;
     terminal.show_cursor()?;
 
-    if let Err(err) = res {
-        println!("{:?}", err)
+    if let Err(err) = res { 
+        println!("{}", err);
     }
-
     Ok(())
 }
