3ed6ad76847a970419aff2fba4c39bff779c6fd5
Troels Dalsgaard Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2024-07-16 23:28:19 +0200

    Refactor: Did a couple of bigger changes
    New idenfitier for the Tree. This is now an enum describing the level
    and the contents of the tree better. This will allow for a calling and methods
    and getting properties.
    
    The tree binary has been moved to examples. It was not a useful program, and it shadows
    the ubiquitous tree that show a file tree.
    
    Added logging with the tracing crate. This is helpful when debugging issues in the tui
    
    Updated Cargo.lock

--- src/bin/dtui/stateful_tree.rs
+++ src/bin/dtui/stateful_tree.rs
@@ -1,18 +1,49 @@
-use tui_tree_widget::{TreeItem, TreeState};
+use std::collections::HashMap;
 
-pub struct StatefulTree<'a> {
-    pub state: TreeState<usize>,
-    pub items: Vec<TreeItem<'a, usize>>,
+use itertools::Itertools;
+use tui_tree_widget::{TreeItem, TreeState};
+use zbus_xml::{ArgDirection, Node};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum MemberTypes {
+    Methods,
+    Properties,
+    Signals,
+}
+// This enum encodes where we are in the GUI tree
+// > Objects
+//  > Interfaces
+//   > Member (Aka one of Method/Property/Signal)
+//    > Methods/Properties/Signals (The actual list of the methods)
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum DbusIdentifier {
+    Object(String),      // ObjectPath
+    Interface(String),   // InterfaceName
+    Member(MemberTypes), // Can be Method, Properties, Signals
+    Method(String),      // zbus_name::MemberName
+    Property(String),    // zbus_name::PropertyName
+    Signal(String),      // zbus_name::MemberName
 }
 
-impl<'a> Default for StatefulTree<'a> {
+impl Default for DbusIdentifier {
+    fn default() -> Self {
+        DbusIdentifier::Object("/".to_string())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct StatefulTree {
+    pub state: TreeState<DbusIdentifier>,
+    pub items: Vec<TreeItem<'static, DbusIdentifier>>,
+}
+
+impl Default for StatefulTree {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<'a> StatefulTree<'a> {
-    #[allow(dead_code)]
+impl StatefulTree {
     pub fn new() -> Self {
         Self {
             state: TreeState::default(),
@@ -20,10 +51,23 @@ impl<'a> StatefulTree<'a> {
         }
     }
 
-    pub fn with_items(items: Vec<TreeItem<'a, usize>>) -> Self {
+    pub fn from_nodes(nodes: HashMap<String, Node<'static>>) -> Self {
+        let items = nodes
+            .iter()
+            .sorted_by(|a, b| a.0.cmp(b.0))
+            .map(|(object_name, node)| -> TreeItem<DbusIdentifier> {
+                let children = node_to_treeitems(node);
+                TreeItem::new(
+                    DbusIdentifier::Object(object_name.clone()),
+                    object_name.clone(),
+                    children,
+                )
+                .unwrap()
+            })
+            .collect();
         Self {
             state: TreeState::default(),
-            items,
+            items: items,
         }
     }
 
@@ -46,4 +90,112 @@ impl<'a> StatefulTree<'a> {
     pub fn toggle(&mut self) {
         self.state.toggle_selected();
     }
+}
+
+fn node_to_treeitems(node: &zbus_xml::Node) -> Vec<TreeItem<'static, DbusIdentifier>> {
+    let children: Vec<TreeItem<DbusIdentifier>> = node
+        .interfaces()
+        .iter()
+        .map(|interface| {
+            let methods: Vec<TreeItem<DbusIdentifier>> = interface
+                .methods()
+                .iter()
+                .map(|method| {
+                    let inputs: Vec<String> = method
+                        .args()
+                        .iter()
+                        .filter(|arg| arg.direction().is_some_and(|s| s == ArgDirection::In))
+                        .map(|arg| format!("{}: {}", arg.name().unwrap_or_default(), arg.ty()))
+                        .collect();
+                    let outputs: Vec<String> = method
+                        .args()
+                        .iter()
+                        .filter(|arg| arg.direction().is_some_and(|s| s == ArgDirection::Out))
+                        .map(|arg| format!("{}: {}", arg.name().unwrap_or_default(), arg.ty()))
+                        .collect();
+                    let return_arrow = if outputs.is_empty() { "" } else { "=>" }; // If we dont return anything, the arrow shouldnt be there
+                    let leaf_string: String = format!(
+                        "{}({}) {} {}",
+                        method.name(),
+                        inputs.join(", "),
+                        return_arrow,
+                        outputs.join(", ")
+                    );
+                    TreeItem::new_leaf(
+                        DbusIdentifier::Method(method.name().to_string()),
+                        leaf_string,
+                    )
+                })
+                .collect();
+            let properties: Vec<TreeItem<DbusIdentifier>> = interface
+                .properties()
+                .iter()
+                .map(|property| {
+                    TreeItem::new_leaf(
+                        DbusIdentifier::Property(property.name().to_string()),
+                        format!("{}: {}", property.name(), property.ty()),
+                    )
+                })
+                .collect();
+            let signals: Vec<TreeItem<DbusIdentifier>> = interface
+                .signals()
+                .iter()
+                .map(|signal| {
+                    // Signals can only have input parameters
+                    let inputs: Vec<String> = signal
+                        .args()
+                        .iter()
+                        .filter(|arg| arg.direction().is_some_and(|s| s == ArgDirection::In))
+                        .map(|arg| format!("{}: {}", arg.name().unwrap_or_default(), arg.ty()))
+                        .collect();
+                    let leaf_string: String = format!("{}({})", signal.name(), inputs.join(", "));
+                    TreeItem::new_leaf(
+                        DbusIdentifier::Signal(signal.name().to_string()),
+                        leaf_string,
+                    )
+                })
+                .collect();
+            // let annotations: Vec<TreeItem> = interface
+            //     .annotations()
+            //     .iter()
+            //     .map(|annotation| {
+            //         TreeItem::new_leaf(annotation.name().to_string())
+            //     })
+            //     .collect();
+            let methods_tree = TreeItem::new(
+                DbusIdentifier::Member(MemberTypes::Methods),
+                "Methods",
+                methods,
+            )
+            .expect("Methods should have different ids");
+            let properties_tree = TreeItem::new(
+                DbusIdentifier::Member(MemberTypes::Properties),
+                "Properties",
+                properties,
+            )
+            .expect("Properties should have different ids");
+            let signals_tree = TreeItem::new(
+                DbusIdentifier::Member(MemberTypes::Signals),
+                "Signals",
+                signals,
+            )
+            .expect("Signals should have different ids");
+            // let annotations_tree =
+            //     TreeItem::new("Annotations", annotations);
+            // TODO: Annotations are used differently, so i dont want to waste space with it
+            TreeItem::new(
+                DbusIdentifier::Interface(interface.name().to_string()),
+                interface.name().to_string(),
+                vec![
+                    methods_tree,
+                    properties_tree,
+                    signals_tree,
+                    // annotations_tree,
+                ],
+            )
+            .unwrap()
+        })
+        .collect();
+
+    children
 }
